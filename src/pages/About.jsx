291e25341612c4ba8e55ebfa6972dd03646a1417
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-08-13 22:42:42 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,8 @@ function About(){
         <>
         <Navbar/>
          <h1 id="x">Welcome to About Page</h1>
+
+
         <Link  href="/">Go Back</Link>
         <Image src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAzMCBBcHI%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00343983-ygslxsbbzf-portrait.jpg" width="210" height="310" alt="test img"/>
         </>
