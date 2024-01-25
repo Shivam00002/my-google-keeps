6f1fc0084aed1b95b8a00f83cd5e067d77f83580
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-01-25 23:44:30 +0530

    Update Loading.tsx

--- client/src/components/common/Loading.tsx
+++ client/src/components/common/Loading.tsx
@@ -4,7 +4,8 @@ import React, { FC } from "react";
 const Loading: FC = () => {
   return (
     <div>
-      <Image src={"/loading.gif"} height={32} width={32} alt="loading" />
+      <Image src={"/loading.gif"}
+ height={32} width={32} alt="loading" />
     </div>
   );
 };
